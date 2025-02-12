5011ed0561d63819cd10b06f96f5db59e5671e3f
Y3HGUY <tjyn7m@umsystem.edu>
2025-02-12 14:05:58 -0600

    Generated Getters and setters

--- Assignment1/src/library/student/Book.java
+++ Assignment1/src/library/student/Book.java
@@ -13,7 +13,40 @@ public class Book {
 	this.Author = Author;
 	this.ISBN = "Unknown";
 	this.price = 0.0;
-	
 	}
+	
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAuthor() {
+		return Author;
+	}
+
+	public void setAuthor(String author) {
+		Author = author;
+	}
+
+	public String getISBN() {
+		return ISBN;
+	}
+
+	public void setISBN(String iSBN) {
+		ISBN = iSBN;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	
 	
 }
